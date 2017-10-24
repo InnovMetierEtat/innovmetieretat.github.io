92bb261a8c593b46ed2866852f928c58194573b2
Gabriel <dehan.gabriel@gmail.com>
2017-10-24 11:29:06 +0200

    typo

--- webpack/lib/github.js
+++ webpack/lib/github.js
@@ -1,6 +1,5 @@
 // We add an |apie| salt to the token so github doesn't delete it automaticaly (security measure)
-const token = "dc|apie|534d6bb2a585a58910299bb3a4edc0cee18c|apie|dc";
-
+const token = "dc|apie|534d6bb2a585a58910299bb3a4edc0cee18c|apie|0f";
 
 // Removes the salt added to the token
 const desalinize = (function(t) { return t.replace("|apie|", "").replace("|apie|", ""); });
